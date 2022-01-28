f581386e2b9f6cd92826e5ad95564cd086901fda
Kaitlyn Kolbow <73507371+knkolbow@users.noreply.github.com>
2022-01-28 13:10:18 -0600

    Update Perimeter.h

--- Perimeter.h
+++ Perimeter.h
@@ -19,26 +19,23 @@ int calc_perimeter(const std::vector<std::vector<int> >& islandPerimeter) {
 	for (int i = 0; i < islandPerimeter.size(); i++) {
 		for (int j = 0; j < islandPerimeter[i].size(); j++) {
 			if (islandPerimeter[i][j] == 1) {
-				int count = 0;
 
 				//Check if cell is in top row OR has 0 above
 				if (i == 0 || islandPerimeter[i - 1][j] == 0) {
-					count++;
+					perimeter++;
 				}
 				//Check if cell is in the bottom row or has 0 below
 				if (i == islandPerimeter.size() - 1 || islandPerimeter[i + 1][j] == 0) {
-					count++;
+					perimeter++;
 				}
 				//Check if cell is all the way to the left or has 0 to the left
 				if (j == 0 || islandPerimeter[i][j - 1] == 0) {
-					count++;
+					perimeter++;
 				}
 				//Check if cell is all the way to the right or has 0 to the right
 				if (j == islandPerimeter[i].size() - 1 || islandPerimeter[i][j + 1] == 0) {
-					count++;
+					perimeter++;
 				}
-				//add count to the perimeter
-				perimeter += count;
 			}
 		}
 	}
